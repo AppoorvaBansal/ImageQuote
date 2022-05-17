26b3efd00ce82850bff2f8b6d8f5504d2743d731
Appoorva Bansal <appoorva.ag104@gmail.com>
2022-05-17 12:02:44 +0530

    Commit

--- app/src/androidTest/java/com/example/affirmations/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/affirmations/ExampleInstrumentedTest.kt
@@ -1,5 +1,6 @@
 package com.example.affirmations
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +8,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,6 +16,9 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
+
+//@get:Rule
+//val activity = ActivityScenarioRule(MainActivity::class.java)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
